3145c9a3e6f9784421594319ea97e4bf3ab75330
aldousalde <85655422+aldousalde@users.noreply.github.com>
2021-08-28 19:07:36 +0800

    Changed submit array method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   // items = ['Angular 4', 'React', 'Underscore'];
@@ -16,33 +16,33 @@ export class AppComponent {
       email: 'fern.castro@ttec-demo.com',
       rating: 5,
       review:
-        'Stunning and awesome gameplay. The game might be old, but it has stood the test of time.'
+        'Stunning and awesome gameplay. The game might be old, but it has stood the test of time.',
     },
     {
       fullName: 'Sophia Parsons',
       email: 'sophia.parsons@ttec-demo.com',
       rating: 4,
-      review: 'Top-notch. Got a few life lessons here and there.'
+      review: 'Top-notch. Got a few life lessons here and there.',
     },
     {
       fullName: 'Trista Porter',
       email: 'trista.porter@ttec-demo.com',
       rating: 3,
-      review: 'Really good game, overrated though.'
+      review: 'Really good game, overrated though.',
     },
     {
       fullName: 'Percival Conner',
       email: 'percival.conner@ttec-demo.com',
       rating: 5,
-      review: 'Buying this below the full price was a mistake.'
+      review: 'Buying this below the full price was a mistake.',
     },
     {
       fullName: 'Ellen Swanson',
       email: 'ellen.swansons@ttec-demo.com',
       rating: 5,
       review:
-        '2 decades of gaming, this is the greatest game I have ever played and will ever play.'
-    }
+        '2 decades of gaming, this is the greatest game I have ever played and will ever play.',
+    },
   ];
 
   getValues(val) {
@@ -57,7 +57,7 @@ export class AppComponent {
       newItem.rating !== 'Rating' &&
       newItem.review !== ''
     ) {
-      this.items.push(newItem);
+      this.items.unshift(newItem);
     } else {
     }
   }
